808ec9109bcd6dc6d9e44e8248a297c477a1db15
vSsongv <songdh21@naver.com>
2022-05-09 10:07:13 +0900

    ✨ start_RN

---
+++ ch02_1/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {SafeAreaView, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>'Hello World'</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+export default App;
