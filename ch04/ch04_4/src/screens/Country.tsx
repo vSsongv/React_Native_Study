ce2160d27b072439dc1be58ec9f078025e03b60e
vSsongv <songdh21@naver.com>
2022-06-04 04:39:11 +0900

    ✅Country

---
+++ ch04/ch04_4/src/screens/Country.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import type {FC} from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+import * as D from '../data';
+
+export type CountryProps = {
+  country: D.ICountry;
+};
+
+const Country: FC<CountryProps> = ({country}) => {
+  const {name, population, subregion, region} = country;
+
+  return (
+    <View style={[styles.view]}>
+      <View>
+        <Text style={styles.name}>{name}</Text>
+      </View>
+      <View>
+        <Text>population: {population}</Text>
+        <Text>subregion: {subregion}</Text>
+        <Text>region: {region}</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  view: {padding: 5},
+  name: {fontSize: 30, fontWeight: '400'},
+});
+
+export default Country;
